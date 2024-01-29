6c2c68fb40b8ee37aae82604c9fe5a370ac81c2d
Gabriel Delgado <6327@holbertonstudents.com>
2024-01-29 16:34:47 +0000

    Adding 'Poppins' font

--- tailwind.config.mjs
+++ tailwind.config.mjs
@@ -1,8 +1,12 @@
 /** @type {import('tailwindcss').Config} */
+const defaultTheme = require("tailwindcss/defaultTheme");
 export default {
 	content: ['./src/**/*.{astro,html,js,jsx,md,mdx,svelte,ts,tsx,vue}'],
 	theme: {
 		extend: {
+			fontFamily: {
+				sans: ['Poppins', ...defaultTheme.fontFamily.sans],
+			},
 			colors: {
 				'vgreen': {
 					'50': '#f5f8f6',
