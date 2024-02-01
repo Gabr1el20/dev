d01437534f93864f5a9db9eec66a70dfdaa5f4d2
Gabr1el20 <6327@holbertonstudents.com>
2024-02-01 17:01:26 -0300

    Added sakuga page

---
+++ src/components/SakugaRetriever.tsx
@@ -0,0 +1,25 @@
+import { useEffect, useState } from "react"
+import {getCuts} from "../utils/getCuts.ts";
+interface Cut {
+    series: string,
+    "author(s)": string,
+    source: string,
+    cut_id: string,
+    url: string
+}
+export default function SakugaRetriever() {
+    const [data, setData] = useState({} as Cut)
+    useEffect(() => {
+        getCuts().then((data) => setData(data))
+    }, [])
+    return (
+        <>
+         <img src={data.url} alt="" className="w-5/6 md:col-start-1 mx-auto rounded-md h-full col-span-2 row-span-2"/>
+        <article className="mx-auto shadow-xl w-4/5 md:w-3/5 flex flex-col md:col-start-3 md:row-start-2 md:row-span-2 justify-around items-center rounded-lg bg-mongoose-600 col-span-2 text-mongoose-200">
+            <h2>{`From: ${data.series}, ${data.source}`}</h2>
+            <h3>{`Author(s): ${data["author(s)"]}`}</h3>
+            <button onClick={() => getCuts().then(data => setData(data))} className="py-2 px-4 rounded-xl shadow-xl active:bg-mongoose-800 hover:scale-110 transition hover:text-mongoose-100 border-2 border-mongoose-200">Get Another cut!</button>
+        </article>
+        </>
+    )
+}
---
+++ src/utils/getCuts.ts
@@ -0,0 +1,4 @@
+const API_URL: string = "https://oq5npnwovb.execute-api.us-east-1.amazonaws.com/default/randomSakuga";
+export const getCuts = () => {
+    return fetch(API_URL).then((res) => res.json())
+}
